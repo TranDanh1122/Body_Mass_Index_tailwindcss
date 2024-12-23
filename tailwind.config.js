fdc6f73c5836a13ad8872c0481b63bcfc0adce5c
TranDanh1122 <trandanh14042000@gmail.com>
2024-12-23 17:09:40 +0700

    hero section

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "blue": "#345FF6",
+        "gunmetal": "#253347",
+        "dark_electric_blue": "#5E6E85",
+        "white": "#fff",
+        "red": "#F21E84",
+        "orange": "#F69134",
+        "green": "#22C1C1",
+        "pupple": "#9747FF",
+        "yellow": "#FFC700",
+        "gradient_top" : "rgba(214, 252, 254, 0)",
+        "gradient_bot" : "rgba(214, 230, 254, 1)"
+
+      }
+    },
+  },
+  plugins: [],
+}
+
